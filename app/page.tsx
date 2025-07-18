d61707ef423726d6daeafe6f1c4fc92df1ef2196
Pinninti Pavan <pinnintipavan04@gmail.com>
2025-07-18 14:35:47 +0530

    Implemented Filters

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { CarCard, CustomFilter } from "@/components";
 import Hero from "@/components/Hero";
 import SearchBar from "@/components/SearchBar";
+import { fuels, yearsOfProduction } from "@/constants";
 import { HomeProps } from "@/types";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
@@ -30,8 +31,8 @@ export default async function Home({searchParams}: HomeProps) {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels}/>
+            <CustomFilter title="year" options={yearsOfProduction}/>
           </div>
         </div>
 
