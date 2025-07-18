2b6c06244d145f7a4871885d2482b19c6b3f84e0
Pinninti Pavan <pinnintipavan04@gmail.com>
2025-07-18 15:33:22 +0530

    Implemented Show More

--- components/index.ts
+++ components/index.ts
@@ -7,7 +7,8 @@ import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
 import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
 
 export {
-    Hero, CustomButton, NavBar, Footer, SearchBar, CustomFilter, SearchManufacturer, CarCard, CarDetails
+    Hero, CustomButton, NavBar, Footer, SearchBar, CustomFilter, SearchManufacturer, CarCard, CarDetails, ShowMore
 }
