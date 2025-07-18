b609f972f2389f1d75f843ad2558e48312e1923e
Pinninti Pavan <pinnintipavan04@gmail.com>
2025-07-18 13:57:39 +0530

    Implemented Search

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+    const {manufacturer, year, model, limit, fuel} = filters
+
     const headers = {
 		'x-rapidapi-key': 'd3de16392fmsh003cea5fd40c701p168d99jsn3c4d89869a22',
 		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model={model}&limit=${limit}&fuel_type=${fuel}`, {
         headers: headers,
     });
 
